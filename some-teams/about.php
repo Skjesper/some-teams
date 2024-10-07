8665773f2e6620f87b7234b404b457fbf7d1c0ce
Skjesper <jesperskeppstedt@gmail.com>
2024-10-07 17:42:15 +0200

    working on adding a img src

--- some-teams/about.php
+++ some-teams/about.php
@@ -4,10 +4,11 @@ require_once __DIR__ . "/header.php"; ?>
 
 <article class="textAboutBox">
   <h2>Who am I?</h2>
+  <h3>That's one secret I'll never tell... xoxo</h3>
   <p>
-    That's one secret I'll never tell... xoxo
-    name is Jesper and I'm the creator of some-teams. Hope you find the site useful.
+    Okey the cat is out of the bag.. It's me Jesper Skeppstedt. The creator of some teams. Hope you find the site useful.
   </p>
+  <img src="file:///C:/Users/Jesper/Documents/GitHub/some-teams/some-teams/resources/logo.svg" width="200px" height="200px">
 
 </article>
 

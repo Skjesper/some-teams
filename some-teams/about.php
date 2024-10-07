4ebdbe6706d57edb28b21d27cb7d3ff8eb63c16d
Skjesper <jesperskeppstedt@gmail.com>
2024-10-07 16:41:33 +0200

    Still working on the about page

--- some-teams/about.php
+++ some-teams/about.php
@@ -4,7 +4,9 @@ require_once __DIR__ . "/header.php"; ?>
 
 <article class="textAboutBox">
   <h2>Who am I?</h2>
-  <p>My name is Jesper and I'm the creator of some-teams. Hope you find the site useful.
+  <p>
+    That's one secret I'll never tell... xoxo
+    name is Jesper and I'm the creator of some-teams. Hope you find the site useful.
   </p>
 
 </article>

8e6b1fc52bd2fb28cf1f858c8f22745d4f78385e
Skjesper <jesperskeppstedt@gmail.com>
2024-10-07 16:25:05 +0200

    working on aboutpage

--- some-teams/about.php
+++ some-teams/about.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . "/header.php"; ?>
 
-<article class="about">
+<article class="textAboutBox">
+  <h2>Who am I?</h2>
   <p>My name is Jesper and I'm the creator of some-teams. Hope you find the site useful.
   </p>
 
--- some-teams/footer.php
+++ some-teams/footer.php
@@ -1,7 +1,10 @@
 </main>
 </body>
-<div class="footerGrid">
-  <footer>Jesper Skeppstedt est 1993</footer>
-</div>
+
+<footer>
+  <h3>Jesper Skeppstedt est 1993
+  </h3>
+
+</footer>
 
 </html>
